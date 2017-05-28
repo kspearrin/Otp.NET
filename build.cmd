2cf90883f7a6d49928dc87296384f8fcdf7e89a4
Tommy Parnell <tparnell8@gmail.com>
2017-05-28 13:11:25 -0700

    update to new csproj

--- build.cmd
+++ build.cmd
@@ -2,18 +2,8 @@
 cd %~dp0
 
 SETLOCAL
-SET CACHED_NUGET=%LocalAppData%\NuGet\NuGet.exe
-
-IF EXIST %CACHED_NUGET% goto copynuget
-IF NOT EXIST %LocalAppData%\NuGet md %LocalAppData%\NuGet
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest 'https://www.nuget.org/nuget.exe' -OutFile '%CACHED_NUGET%'"
-
-:copynuget
-IF EXIST .nuget\nuget.exe goto build
-md .nuget
-copy %CACHED_NUGET% .nuget\nuget.exe > nul
 
 :build
-call npm install -g gulp
+call npm install -g gulp-cli
 call npm install
 call gulp
---
+++ src/Otp.NET.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Otp.NET.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
